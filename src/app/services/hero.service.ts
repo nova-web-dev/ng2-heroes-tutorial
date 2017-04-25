a4639c1a4a97e2d293adbf59f9ef8e26ef919798
Carl Eiserman <carleiserman@gmail.com>
2017-04-25 08:36:52 +0100

    feature: added promise for data, delayed data 2 seconds

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -7,6 +7,13 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroService {
     getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+        return Promise.resolve( HEROES );
+    }
+
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise(resolve => {
+            // Simulate server latency with 2 second delay
+            setTimeout(() => resolve(this.getHeroes()), 2000);
+        });
     }
 }
