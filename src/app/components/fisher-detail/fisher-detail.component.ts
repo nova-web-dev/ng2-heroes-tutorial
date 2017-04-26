85aa207feea1baa0a5dabb1db8e15845f0e356ce
Carl Eiserman <carleiserman@gmail.com>
2017-04-26 13:23:43 +0100

    feature: added better view for catch screen:

--- src/app/components/fisher-detail/fisher-detail.component.ts
+++ src/app/components/fisher-detail/fisher-detail.component.ts
@@ -25,11 +25,15 @@ export class FisherDetailComponent {
     lat: number;
     lng: number;
 
+    lorem =  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
     constructor(
         private fisherService: FisherService,
         private route: ActivatedRoute,
         private location: Location,
-        private router: Router
+        private router: Router,
+
+
     ) {}
 
     ngOnInit(): void {
@@ -51,4 +55,6 @@ export class FisherDetailComponent {
     goBack(): void {
         this.location.back();
     }
+
+
 }
